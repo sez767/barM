df30bccb730c366a4c14aff219d1a8d04a9e959f
sez767 <sez767@gmail.com>
2021-05-13 17:07:29 +0300

    fx

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-// phpinfo();
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 
 header('Content-Type: text/html; charset=utf-8');
 // require_once (dirname(__FILE__) . "/lib/db.php");
