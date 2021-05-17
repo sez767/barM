6ad4085ceb00f4bf61b2901c9feb8d168804a1ba
sez767 <sez767@gmail.com>
2021-05-17 13:31:43 +0300

    fx

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ERROR);
+error_reporting(0);
 ini_set('display_errors', 0);
 
 header('Content-Type: text/html; charset=utf-8');
--- lib/db.php
+++ lib/db.php
@@ -1,7 +1,7 @@
 <?php
 
 ini_set("display_errors", 0);
-error_reporting(E_ERROR);
+error_reporting(0);
 
 ini_set('ignore_repeated_errors', 'On');
 
