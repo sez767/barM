5b2627c3344fbfab68424c725457b9c46bb55fb6
sez767 <sez767@gmail.com>
2021-06-04 10:12:20 +0300

    ff

--- lib/db.php
+++ lib/db.php
@@ -8,12 +8,12 @@ ini_set('ignore_repeated_errors', 'On');
 session_start();
 
 
-require_once dirname(__FILE__) . '/../ini/Application.php';
-require_once dirname(__FILE__) . '("/../DevExtreme/LoadHelper.php")';
-spl_autoload_register(array("DevExtreme\LoadHelper", "LoadModule"));
+// require_once dirname(__FILE__) . '/../ini/Application.php';
+// require_once dirname(__FILE__) . '("/../DevExtreme/LoadHelper.php")';
+// spl_autoload_register(array("DevExtreme\LoadHelper", "LoadModule"));
 
-use DevExtreme\DbSet;
-use DevExtreme\DataSourceLoader;
+// use DevExtreme\DbSet;
+// use DevExtreme\DataSourceLoader;
 
 function db_connect() {
     global $db_link_ref;
