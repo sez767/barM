c90272f27d4ba0fad8a96e98c9f6113b58812925
sez767 <sez767@gmail.com>
2021-06-04 10:40:00 +0300

    ff

--- handlers/get_menu_obzvon.php
+++ handlers/get_menu_obzvon.php
@@ -249,8 +249,12 @@ if (!empty($_GET['id'])) {
     // collect request parameters
     $start = (int) isset($_REQUEST['skip']) ? $_REQUEST['skip'] : 0;
     $count = (int) isset($_REQUEST['take']) ? $_REQUEST['take'] : 100;
-    $sort = isset($_REQUEST['sort']) ? $_REQUEST['sort'] : '';
-    $dir = isset($_REQUEST['desc']) ? $_REQUEST['desc'] : 'DESC';
+    if(isset($_REQUEST['sort'])) {
+        $presort = json_decode($_REQUEST['sort']), true);
+    }
+    $sort = isset($presort['selector']) ? $presort['selector'] : '';
+    $dir = ($presort['desc']) ? 'DESC' : 'ASC';
+    // var_dump($sort);
     $filters = isset($_REQUEST['filter']) ? $_REQUEST['filter'] : null;
     $sort = my_mysqli_real_escape_string($sort);
     $dir = my_mysqli_real_escape_string($dir);
@@ -436,12 +440,14 @@ if (!empty($_GET['id'])) {
     $queryTotal = "SELECT COUNT(id) AS obzvon_total FROM staff_order WHERE country IN (" . $_SESSION['country'] . ") $all_in_condition AND $where $str_add";
     $queryTotal = "SELECT FOUND_ROWS() AS obzvon_total ";
 
-   /* if ($sort != "") {
+   if ($sort != "") {
         $query .= " ORDER BY `" . $sort . "` " . $dir;
     } else {
         
-    }*/
+    
     $query .= " ORDER BY `date` DESC ";
+    }
+
     $query .= " LIMIT $start, $count";
 
     ApiLogger::addLogVarExport('START');
